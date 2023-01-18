bb28e7c4ad770525d0debdd09ab1c7df892d1443
Alejandro-Delacruz <118419015+Alejandro-Delacruz@users.noreply.github.com>
2023-01-17 22:22:41 -0600

    Final Results

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,26 +7,24 @@ Created on Tue Jan 17 17:51:59 2023
 
 import pandas as pd
 df = pd.read_csv ("C:/Users/avo01/Documents/GitHub/python-challenge/PyBank/budget_data.csv")
-print("Finacial Analysis\n")
-print("----------------------------")
+
 #The total number of months included in the dataset
 total_number_of_months=len(df["Date"])
-print("Total Months: "+str(total_number_of_months))
+
 
 #The net total amount of "Profit/Losses" over the entire period
 net_total_amount=df["Profit/Losses"].sum()
-print("Total: "+str(net_total_amount))
+
 
 #The changes in "Profit/Losses" over the entire period, and then the average of those changes
 changes=df["Profit/Losses"]-df["Profit/Losses"].shift(1)
 df["changes"]=changes
-average_change=df["changes"].mean()
-print("Average Change:" + str(average_change))
+average_change=round(df["changes"].mean(),2)
+
 
 #The greatest increase in profits (date and amount) over the entire period
 max_increase=df["changes"].max()
 max_decrease=df["changes"].min()
-
 
 max_increase_df=df[df["changes"]==max_increase]
 increase_date=list(max_increase_df["Date"])[0]
@@ -34,5 +32,19 @@ increase_value=list(max_increase_df["changes"])[0]
 max_decrease_df=df[df["changes"]==max_decrease]
 decrease_date=list(max_decrease_df["Date"])[0]
 decrease_value=list(max_decrease_df["changes"])[0]
-print("Greatest Increase in Profits:",increase_date,"{:.0f}".format(increase_value))
-print("Greatest Decrease in Profits:",decrease_date,"{:.0f}".format(decrease_value))
+
+
+with open('C:/Users/avo01/Documents/GitHub/python-challenge/PyBank/Budget results.txt', 'w') as f:
+    f.write("Financial Analysis")
+    f.write("\n")
+    f.write("----------------------------")
+    f.write("\n")
+    f.write("Total Months: "+str(total_number_of_months))
+    f.write("\n")
+    f.write("Total: $"+str(net_total_amount))
+    f.write("\n")
+    f.write("Average Change: $" + str(average_change))
+    f.write("\n")
+    f.write("Greatest Increase in Profits:" + increase_date+"  ($"+"{:.0f}".format(increase_value)+")")
+    f.write("\n")
+    f.write("Greatest Decrease in Profits:"+decrease_date+ "  ($"+"{:.0f}".format(decrease_value)+")")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,33 +7,38 @@ Created on Mon Jan 16 21:45:06 2023
 
 import pandas as pd
 df = pd.read_csv ("C:/Users/avo01/Documents/GitHub/python-challenge/PyPoll/election_data.csv")
-print("Election Results")
-
-print("-------------------------")
 
 #The total number of votes cast
 Total_Votes=len(df["Ballot ID"])
-print("Total Votes: "+str(Total_Votes))
 
-print("-------------------------")
 #A complete list of candidates who received votes
 outcome=df.groupby("Candidate")["Ballot ID"].count()
 outcome_percentage=round(outcome/Total_Votes*100,2)
-
-
-
-print(str(outcome.index[0]) + ":" + str(outcome_percentage[0]) + "% (" + str(outcome[0]) +")" )
-
-print(str(outcome.index[1]) + ":" + str(outcome_percentage[1]) + "% (" + str(outcome[1]) +")" )
-
-print(str(outcome.index[2]) + ":" + str(outcome_percentage[2]) + "% (" + str(outcome[2]) +")" )
 
 winner=outcome.max()
 winner=outcome[outcome==winner]
 winner.index[0]
 
-print("-------------------------")
 
-print("Winner: "+winner.index[0] )
 
-print("-------------------------")
+with open("C:/Users/avo01/Documents/GitHub/python-challenge/PyPoll/Election results.txt", 'w') as f:
+    f.write("Election Results")
+    f.write("\n")
+    f.write("-------------------------")
+    f.write("\n")
+    f.write("Total Votes: "+str(Total_Votes))
+    f.write("\n")
+    f.write("-------------------------")
+    f.write("\n")
+    f.write(str(outcome.index[0]) + ":" + str(outcome_percentage[0]) + "% (" + str(outcome[0]) +")" )
+    f.write("\n")
+    f.write(str(outcome.index[1]) + ":" + str(outcome_percentage[1]) + "% (" + str(outcome[1]) +")" )
+    f.write("\n")
+    f.write(str(outcome.index[2]) + ":" + str(outcome_percentage[2]) + "% (" + str(outcome[2]) +")" )
+    f.write("\n")
+    f.write("-------------------------")
+    f.write("\n")
+    f.write("Winner: "+winner.index[0] )
+    f.write("\n")
+    f.write("-------------------------")
+    f.write("\n")
